6486325afd431150221fe483bd4da4e66cc2ca42
Khvled <45137388+Khvled@users.noreply.github.com>
2018-12-01 17:52:25 +0100

    Add files via upload

---
+++ Dream.js
@@ -0,0 +1,17 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Cyhper Bot Script By : kHvLeD`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Generals-Shop`,"http://twitch.tv/ninja")
+client.user.setStatus("dnd")
+});
+
+
+
+client.login(process.env.TOKEN);// لا تغير فيها شيء
